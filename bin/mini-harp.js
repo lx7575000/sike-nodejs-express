95457e821d46b30e8db39f6432f41b1245a21796
lx7575000 <liu_narcisus@163.com>
2015-05-11 17:03:48 +0800

    step2 可以自由控制想要监听的端口号

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 var createMiniHarp = require("../index.js");
-app = createMiniHarp();
-console.log("Starting mini-harp on http://localhost:4000");
-app.listen(4000);
+var parsedArgv = require('minimist')(process.argv.slice(2));
+
+
+app = createMiniHarp(parsedArgv._);
+var port =  parseInt(parsedArgv._);
+app.listen(port);
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var connect = require('connect');
 
-module.exports = function miniHarp(){
+module.exports = function miniHarp(port){
 	
 	var app = connect();
-
+	if(port){
+		console.log("Starting mini-harp on http://localhost: 4000");
+	}else{
+		console.log("Starting mini-harp on http://localhost: " + port);
+	}
 	return app;
 }
 
