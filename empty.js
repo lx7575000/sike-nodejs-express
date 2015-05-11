b744de03946ccc251d2db82d52f0e8850b05861e
lx7575000 <liu_narcisus@163.com>
2015-05-11 20:19:00 +0800

    step3 需要通过特定路径才能访问当前时间--代码目前都在empty.js当中

--- empty.js
+++ empty.js
@@ -1,6 +1,23 @@
 var connect = require('connect');
+var url = require('url');
+var http = require('http');
 
-var app = connect();
+connect()
+	.use(function (req, res, next){
+	var pathname = url.parse(req.url).pathname;
+	var date = new Date();
+	if(req.method === 'POST' && pathname === '/current-time'){
+		res.end('Now is ' + date + '\n');
+		next();
+	}else{
+		// res.end('Please query the date with POST method and you shoul visit current-time');
+		res.end('Gannot Get ' + pathname + '\n');
+		next();
+	}
+	})
+	.use(function(req, res){
+		res.end('Good bye everyone ...\n');
+	})
+	.listen(4000);
 
-console.log('Starting http server on http://localhost:4000');
-app.listen();  
+console.log('http://localhost:4000/current-time');
