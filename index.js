b4d5055a137637fa2870a05088bb49058ae1e983
lx7575000 <liu_narcisus@163.com>
2015-05-15 00:36:50 +0800

    添加了prevent模块

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var serveStatic = require('serve-static');
 var jade = require('./lib/processor/jade');
 var less = require('./lib/processor/less');
 var rewriteUrl = require('./lib/processor/rewriteUrl');
+var prevent = require('./lib/processor/prevent');
 
 module.exports = function(path){
 	var app = connect();
 	return app.use(rewriteUrl())
+			  .use(prevent())
 			  .use(jade(path))
 			   .use(less(path))
 			  	.use(serveStatic(path));
