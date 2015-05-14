282096d5f447121811a9135b35dc1a4f952b6eac
lx7575000 <liu_narcisus@163.com>
2015-05-14 21:37:56 +0800

    自从看了人家的代码腰不酸了腿不疼了。。自己码起来也有思路了。不过还是要总结变成自己的东西。less模块的中间件完成

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var jade = require('./lib/processor/jade');
-
+var less = require('./lib/processor/less');
 
 module.exports = function(path){
 	var app = connect();
 	return app.use(jade(path))
-			  .use(serveStatic(path));
+			   .use(less(path))
+			  	.use(serveStatic(path));
 }
