ce2b025dd89597c5fd2b9e9d6c1bced09ff84dd1
lx7575000 <liu_narcisus@163.com>
2015-05-15 00:24:27 +0800

    添加了重新定向rewriteUrl的功能。该模块作为中间件的第一个，默认跳转到index.html位置，后面的makeJade模块会对它进行重新编辑或者使用jade模板。

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env nodemon
 var createMiniHarp = require("../index.js");
-var parsedArgv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv._['port'] ? argv._['port'] : 4000;
+var path = argv._[0] ? argv._[0] : process.cwd();//获得地址
 
-var port = parseInt(parsedArgv._);
-var app = createMiniHarp(port); 
-app();
+var app = createMiniHarp(path);
+console.log('Starting mini-harp on http://localhost: ' + port);
+app()
+	.listen(port);
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var jade = require('./lib/processor/jade');
 var less = require('./lib/processor/less');
+var rewriteUrl = require('./lib/processor/rewriteUrl');
 
 module.exports = function(path){
 	var app = connect();
-	return app.use(jade(path))
+	return app.use(rewriteUrl())
+			  .use(jade(path))
 			   .use(less(path))
 			  	.use(serveStatic(path));
 }
